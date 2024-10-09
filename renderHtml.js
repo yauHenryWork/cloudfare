82ee61c6c679bac6975a25434445790a348be657
handsomejack87 <yauhenrywork@gmail.com>
2024-10-09 14:11:31 -0400

    update from git

--- renderHtml.js
+++ renderHtml.js
@@ -10,6 +10,7 @@ function renderHtml(content) {
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>D1</title>
+	<p>hellow from git<p/>
         <link rel="stylesheet" type="text/css" href="https://templates.cloudflareintegrations.com/styles.css">
       </head>
     
